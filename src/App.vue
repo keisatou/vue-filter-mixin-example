1ebf8faae20ca8aa1f27eec7ab6fead8962e3283
keisatou <dummy>
2017-04-23 16:49:56 +0900

    add a local filter

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
+        <p>{{ text | toUpperCase }}</p>
       </div>
     </div>
   </div>
@@ -10,6 +11,16 @@
 
 <script>
 export default {
+  data() {
+    return {
+      text: 'Hello, there!',
+    };
+  },
+  filters: {
+    toUpperCase(value) {
+      return value.toUpperCase();
+    },
+  },
 };
 </script>
 
