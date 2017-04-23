ec1a1b055c6648b292e08e2290d3076b99151ed2
keisatou <dummy>
2017-04-23 18:27:06 +0900

    Mixins are not shared among Components

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
         <p>{{ text | toUpperCase | to-lowercase }}</p>
+        <button @click="fruits.push('Berries')">Add New Item</button>
         <hr>
         <input type="text" v-model="filterText">
         <ul>
